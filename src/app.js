6f64f8b800494ca6827a7caecedf57f29179225d
Rafael <rafaelongo45@gmail.com>
2022-05-29 20:21:03 -0300

    12 try

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use(customersRouter);
 app.use(categoriesRouter);
 
 app.listen(
-  process.env.ṔORT, 
-  () => console.log(chalk.bold.blue(`Servidor rodando na porta ${process.env.PORT}`))
+  process.env.ṔORT || 4000, 
+  console.log(chalk.bold.blue(`Servidor rodando na porta ${process.env.PORT}`))
 )
